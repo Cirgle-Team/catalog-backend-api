4f5a3c9cbdf18d6e97db54e3131b2ab8d1ffede1
Gyumin Bae <86611784+BGMSound@users.noreply.github.com>
2023-11-12 22:39:57 +0900

    refactor: 에러 메시지 수정

--- src/main/kotlin/org/cirgle/catalog/presenter/dto/request/CaffeineMenuDeleteRequest.kt
+++ src/main/kotlin/org/cirgle/catalog/presenter/dto/request/CaffeineMenuDeleteRequest.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank
 
 data class CaffeineMenuDeleteRequest(
     @field:JsonProperty("menu")
-    @field:NotBlank(message = "invalid-001")
+    @field:NotBlank(message = "menu-003")
     private val _menu: String?,
 ) {
     val menu: String get() = _menu!!
